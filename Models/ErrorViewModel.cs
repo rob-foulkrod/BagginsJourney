96098b116a0ba291fc923cd2d657bdcd253e9573
Rob Foulkrod <RobFoulkrod@microsoft.com>
2021-11-01 15:18:09 -0400

    Initial Checkin
    the simple results of dotnet new and a default .gitignore

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace baggins.web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
